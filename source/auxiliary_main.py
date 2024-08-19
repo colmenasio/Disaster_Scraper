ddf1de6c4f9f09710f581614c3c78448200f91a2
Colmenero <a.colmenero.fernandez@gmail.com>
2024-08-19 15:41:08 +0200

    Implemented event merging via disaster

--- source/auxiliary_main.py
+++ source/auxiliary_main.py
@@ -21,9 +21,18 @@ def generate_search_query(self: Event) -> str:
     return human_readable_query.replace(' ', '+')
 
 
-def disaster_to_dict_factory(event_list: list[Event]) -> Callable[[DisasterLinker], dict]:
-    def disaster_to_dict(disaster: DisasterLinker) -> dict:
+def disaster_to_dict_factory(event_list: list[Event]) -> Callable[[DisasterLinker], dict | list[dict]]:
+    def disaster_to_dict(disaster: DisasterLinker) -> list[dict]:
         # Define how to generate a row for the result dataframe from a disaster instace (a cluster of events)
-        pass
-
+        event_groups = {}
+        for event in event_list:
+            if event.df_index not in disaster.indexes:
+                continue
+            if event.theme not in event_groups.keys():
+                event_groups[event.theme] = []
+            event_groups[event.theme] += [event]
+        dict_list = []
+        for theme in event_groups.keys():
+            dict_list.append(Event.combine_events(event_groups[theme]))
+        return dict_list
     return disaster_to_dict
