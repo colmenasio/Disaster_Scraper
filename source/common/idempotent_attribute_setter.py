445a0e8de6631a0daa2598019de33e5045468ffc
Colmenero <a.colmenero.fernandez@gmail.com>
2024-08-20 23:51:29 +0200

    Fixed obvious bug

--- source/common/idempotent_attribute_setter.py
+++ source/common/idempotent_attribute_setter.py
@@ -3,17 +3,17 @@ from typing import Any, Callable
 
 def idempotent_attribute_setter(attribute_name: str):
     """
-    Decorator that only executes the callable if the attribute passed to it is not none
+    Decorator that only executes the callable if the attribute passed to it evaluates to false
 
     Useful for initialization of instances and avoinding redundant calls
     """
 
     def inner(func: Callable[[Any], None]):
         def wrapper(*args, **kwargs):
-            if getattr(args[0], attribute_name) is None:
-                func(*args, **kwargs)
+            if bool(getattr(args[0], attribute_name)):
                 return None
             else:
+                func(*args, **kwargs)
                 return None
 
         return wrapper
