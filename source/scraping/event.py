e1696d75d031a1073af3297c18623a29fd59e80d
Colmenero <a.colmenero.fernandez@gmail.com>
2024-08-20 16:03:46 +0200

    Moved unittest to its own script

--- source/scraping/event.py
+++ source/scraping/event.py
@@ -210,31 +210,3 @@ class Event:
         answers_generator = (a.answers for a in Event.get_articles_iterable(events))
         info["answer_ratio"] = merge_dicts(answers_generator, lambda x: sum(x) / len(x))
         return info
-
-
-if __name__ == '__main__':
-    print("Imports/Initialization: Done\nStarting unit test")
-    Event.INPUT_PATH = "../utility/testfiles/sample_merged_1.csv"
-    Event.MAX_NEWS_PER_EVENT = 6
-    event_list = Event.from_csv()
-
-
-    def generate_query(self: Event) -> str:
-        numero_mes = self.start_time.month
-        mes = ["enero", "febrero", "marzo", "abril", "mayo", "junio",
-               "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"][numero_mes - 1]
-        human_readable_query = unidecode(
-            f"{self.theme} {self.location} {mes}"
-        )
-        return human_readable_query.replace(' ', '+')
-
-
-    for evento in event_list:
-        evento.get_related_news(generate_query)
-    event_list = Event.filter_out_irrelevant_events(event_list)
-    print(len(event_list))
-    if len(event_list) == 0:
-        print("No articles were relevant")
-        exit(0)
-    print(Article.get_combined_severity(event_list[0].related_articles))
-    print(Article.get_answers_true_ratio(event_list[0].related_articles))
