cb9fab3b253caf46c2d92bb73139ceba99f9baed
Colmenero <a.colmenero.fernandez@gmail.com>
2024-08-26 14:50:35 +0200

    Added sloppy algo for processing data by batches

---
+++ source/rushed_code/manual_main.py
@@ -0,0 +1,42 @@
+import json
+
+from data_merger.disaster_merger_1 import ExpedientMerger
+from data_merger.disaster_merger_2 import QuartileCuller
+from data_merger.disaster_merger_3 import DisasterLinker
+
+from scraping.event import Event
+from scraping.questionnaire import Questionnaire
+
+from common.exceptions import IPBanError
+
+from auxiliary_main import culler, disaster_to_dict_factory, generate_search_query
+
+import pandas as pd
+
+
+def process_events_batches():
+    BATCH_SIZE = 3
+    INPUT_PATH = "../input-output/merged_expedients_3.csv"
+    CONTROL_PATH = "rushed_code/control_data/control.json"
+    with open(CONTROL_PATH) as fstream:
+        CONTROL_DATA = json.load(fstream)
+    df = pd.read_csv(INPUT_PATH, parse_dates=["date"])
+    summaries = []
+    try:
+        while CONTROL_DATA["last_row_processed"] < df.shape[0]:
+            event_list = Event.from_dataframe(
+                df.iloc[CONTROL_DATA["last_row_processed"]:(CONTROL_DATA["last_row_processed"] + BATCH_SIZE)]
+            )
+            Event.get_related_news_concurrent(event_list, generate_search_query)
+            event_list = Event.filter_out_irrelevant_events(event_list)
+            summaries.extend([e.summary_event() for e in event_list])
+            CONTROL_DATA["last_row_processed"] += BATCH_SIZE
+    except IPBanError:
+        print("IP banned stopping procedure")
+    finally:
+        file_id = CONTROL_DATA['last_output_file_id']
+        CONTROL_DATA['last_output_file_id'] += 1
+        result_df = pd.DataFrame(data=summaries)
+        result_df.to_csv(f"rushed_code/unmerged_data/result_{file_id}.csv")
+        with open(CONTROL_PATH, "w+") as fstream:
+            json.dump(CONTROL_DATA, fstream)
