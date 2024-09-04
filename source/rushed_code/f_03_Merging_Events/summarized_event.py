3c922974e9bc0498afe3931e78c705e4faebd79b
Colmenero <a.colmenero.fernandez@gmail.com>
2024-09-04 13:11:53 +0200

    Split summarized_event.py into 2 "branches" to support the new reference approach to articles

--- source/rushed_code/f_03_Merging_Events/summarized_event.py
+++ source/rushed_code/f_03_Merging_Events/summarized_event.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
+from typing import Generator
+
+import pandas as pd
+
 from common.merge_dictionaries import merge_dicts
 
 
 class SummarizedEvent:
     """mostly a dict wrapper so that it `kinda` behaves like a Event instance
     and can reuse most of the merging code lmao"""
+
+    ARTICLES = None
 
     def __init__(self, data_arg: dict):
         """
@@ -15,19 +21,13 @@ class SummarizedEvent:
         start_time: datetime64
         end_time: datetime64
         df_index: int
-        sources: list[int]
-
-        LEGACY
         sources: list[str]
         affected_sectors: list[str]
         severity_ratio: dict[str, float]
         answer_ratio: dict[int, float]
         """
-        if list(data_arg.keys()) == ["theme", "start_time", "end_time", "sources", "location", "df_index",
-                                     "affected_sectors", "severity_ratio", "answer_ratio"]:
-            self.version = "legacy"
         if list(data_arg.keys()) == ["theme", "start_time", "end_time", "sources", "location", "df_index"]:
-            self.version = "reference_1.0"
+            self.version = "reference"
         else:
             raise ValueError("Summarized event in the wrong format")
         self.data = data_arg
@@ -38,14 +38,24 @@ class SummarizedEvent:
         else:
             raise AttributeError("'SummarizedEvent' object has no attribute 'event_id'")
 
+    def make_article_generator(self) -> Generator[dict, None, None]:
+        if self.ARTICLES is None:
+            raise ValueError("Articles were not initialized "
+                             "(make sure to run the initialize_referenced_articles class method)")
+        for a_id in self.sources:
+            yield dict(self.ARTICLES.loc[a_id])
+
+    @classmethod
+    def initialize_referenced_articles(cls, filepath):
+        cls.ARTICLES = pd.read_csv(filepath,
+                                   converters={key: lambda x: eval(x) for key in ['sectors', 'severity', 'answers']}
+                                   ).set_index("id")
+
     @staticmethod
     def combine_events(events: list[SummarizedEvent]) -> dict:
         if len(events) == 0:
             raise ValueError("No articles were provided")
         info = {}
-        version = events[0].version
-        if any((e.version != version for e in events)):
-            raise ValueError("Inconsistent versions")
         theme = events[0].theme
         if any((e.theme != theme for e in events)):
             raise ValueError("Inconsistent themes")
@@ -53,15 +63,19 @@ class SummarizedEvent:
         info["locations"] = list({e.location for e in events})
         info["start_time"] = min([e.start_time for e in events])
         info["end_time"] = max([e.end_time for e in events])
-        info["sources"] = list({source for se in events for source in se.sources})
-        info["df_indexes"] = list({int(se.df_index) for se in events})
-        info["event_ids"] = list({int(e.df_index) for e in events})
-        if version == "legacy":
-            info["affected_sectors"] = list({s for e in events for s in e.affected_sectors})
-            severities_generator = (e.severity_ratio for e in events)
-            info["severity_ratio"] = merge_dicts(severities_generator, lambda x: sum(x) / len(x))
-            answers_generator = (e.answer_ratio for e in events)
-            info["answer_ratio"] = merge_dicts(answers_generator, lambda x: sum(x) / len(x))
+        info["sources"] = list({source for e in events for source in e.sources})
+        info["df_indexes"] = list({int(e.df_index) for e in events})
+
+        def min_mean_max(l: list) -> tuple:
+            if len(l) == 0:
+                raise ValueError("The current summarized_event has no values")
+            return max(l), sum(l) / len(l), min(l)
+
+        info["affected_sectors"] = list({s for e in events for a in e.make_article_generator() for s in a["sectors"]})
+        severities_generator = (a["severity"] for e in events for a in e.make_article_generator())
+        info["severity_ratio"] = merge_dicts(severities_generator, min_mean_max)
+        answers_generator = (a["answers"] for e in events for a in e.make_article_generator())
+        info["answer_ratio"] = merge_dicts(answers_generator, min_mean_max)
         return info
 
     def __repr__(self):
